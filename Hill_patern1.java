a5d96f0454d8811e76f885588229960a5170e011
LAKSHMIPRIYANAGARAJAN <47146230+LAKSHMIPRIYANAGARAJAN@users.noreply.github.com>
2019-08-08 20:01:27 +0530

    Create Hill_patern1.java

---
+++ Hill_patern1.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.io.*;
+
+class Hill_pattern1
+{
+	public static void main (String[] args) 
+	{
+		int n=0,sq=0,num=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			num=1;
+		for(int j=0;j<=i;j++)
+		{
+			System.out.print(num+" ");
+			num++;
+		}
+		System.out.println(" ");
+		}
+		}
+	   
+	}
