a078ffd95100b8dfe9b0b180f00a76eb5612f6c8
LAKSHMIPRIYANAGARAJAN <47146230+LAKSHMIPRIYANAGARAJAN@users.noreply.github.com>
2019-08-08 19:31:36 +0530

    Create Print_*and_hi_N_times.java

---
+++ Print_*and_hi_N_times.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Print_*and_hi_N_times
+{
+	public static void main (String[] args) 
+	{
+		int n=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		    System.out.print("*");
+		System.out.println(" ");
+		for(int i=1;i<=n;i++)
+		    System.out.println("hi ");
+	
+	}
+}
