76a687d7a55a91fb78328ba28d3e0fec828ca0df
LAKSHMIPRIYANAGARAJAN <47146230+LAKSHMIPRIYANAGARAJAN@users.noreply.github.com>
2019-08-08 19:18:50 +0530

    Create fibonacci_series.java

---
+++ fibonacci_series.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+class fibonacci_series
+{
+	public static void main (String[] args) 
+	{
+		int a=0,b=1,c=0,n=0;
+		Scanner sc= new Scanner(System.in);
+		n=sc.nextInt();
+		System.out.print(a+" "+b);
+		for(int i=2;i<n;++i)
+		{
+			c=a+b;
+			System.out.print(" "+c);
+			a=b;
+			b=c;
+		}
+		
+	}
+}
