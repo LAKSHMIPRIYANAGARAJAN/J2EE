f94713975a5fda649aba88f8f05bfdf34812acbc
LAKSHMIPRIYANAGARAJAN <47146230+LAKSHMIPRIYANAGARAJAN@users.noreply.github.com>
2019-08-11 15:23:30 +0530

    Create count_unique_digit_in_number.java

---
+++ count_unique_digit_in_number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+class count_unique_digit_in_number
+{
+	public static void main (String[] args) 
+		int input1=0,r=0,count=0;
+		Scanner sc=new Scanner(System.in);
+		input1=sc.nextInt();
+		int a[]=new int[10];
+		while(input1>0)
+		{
+			r=input1%10;
+			input1=input1/10;
+			a[r]=a[r]+1;
+		}
+		for(int i=0;i<10;i++)
+		{
+			if(a[i]>=1)
+			{
+				count=count+1;
+			}
+		}
+		System.out.println(count);
+	}
+}
